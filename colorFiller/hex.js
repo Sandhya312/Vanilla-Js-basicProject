62413b876336251a2f80b66790db47d43433d8ab
sandhya <sandhyakumari3122003@gmail.com>
2022-04-13 22:32:34 +0530

    Basic Project of Js

---
+++ colorFiller/hex.js
@@ -0,0 +1,18 @@
+let hex = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+
+document.getElementById('btn').addEventListener('click',ChangeBackg);
+
+function ChangeBackg(){
+    let span = document.querySelector('span');
+    let hexa= '#';
+    for(let i = 0; i<6;i++){
+        hexa += hex[getRandomNum()];
+    }
+    console.log(hexa);
+    span.innerHTML = hexa;
+    document.body.style.backgroundColor=hexa;
+}
+
+function getRandomNum(){
+    return Math.floor(Math.random()*hex.length);
+}
---
+++ colorFiller/index.js
@@ -0,0 +1,18 @@
+let color = [ "red", "green","blue","pink","azure","beige"];
+
+document.getElementById('button').addEventListener('click', backColor);
+
+function backColor(){
+    let span = document.querySelector('span');
+    let randomNum = getRandomNum();
+    span.innerText = color[randomNum];
+    console.log(randomNum);
+    document.body.style.backgroundColor=color[randomNum];;
+}
+
+function getRandomNum(){
+  
+   return Math.floor(Math.random()* color.length);
+}
+
+
