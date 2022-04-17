182b397af797bf38d031cfbdabfb08d9d15889f5
sandhya <sandhyakumari3122003@gmail.com>
2022-04-17 23:40:09 +0530

    Delete: delete unsuseful spaces and code

--- colorFiller/hex.js
+++ colorFiller/hex.js
@@ -1,7 +1,5 @@
 let hex = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
-
 document.getElementById('btn').addEventListener('click',ChangeBackg);
-
 function ChangeBackg(){
     let span = document.querySelector('span');
     let hexa= '#';
@@ -12,7 +10,6 @@ function ChangeBackg(){
     span.innerHTML = hexa;
     document.body.style.backgroundColor=hexa;
 }
-
 function getRandomNum(){
     return Math.floor(Math.random()*hex.length);
 }
--- colorFiller/index.js
+++ colorFiller/index.js
@@ -1,7 +1,5 @@
 let color = [ "red", "green","blue","pink","azure","beige"];
-
 document.getElementById('button').addEventListener('click', backColor);
-
 function backColor(){
     let span = document.querySelector('span');
     let randomNum = getRandomNum();
@@ -9,9 +7,7 @@ function backColor(){
     // console.log(randomNum);
     document.body.style.backgroundColor=color[randomNum];;
 }
-
 function getRandomNum(){
-  
    return Math.floor(Math.random()* color.length);
 }
 
